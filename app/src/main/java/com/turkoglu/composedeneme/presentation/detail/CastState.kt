fcd24543518f121c9e9a43627e3ec42871aa9fc0
Salih TÜRKOĞLU <77458433+SalihT95@users.noreply.github.com>
2024-01-06 21:04:00 +0300

    cast added

--- app/src/main/java/com/turkoglu/composedeneme/presentation/detail/CastState.kt
+++ app/src/main/java/com/turkoglu/composedeneme/presentation/detail/CastState.kt
@@ -1,16 +1,11 @@
 package com.turkoglu.composedeneme.presentation.detail
 
+import android.os.Parcelable
+import com.turkoglu.composedeneme.data.remote.dto.Cast
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class CastState(
-    val adult: Boolean = false,
-    val gender: Int = 0 ,
-    val id: Int = 0,
-    val known_for_department: String = "",
-    val name: String = "",
-    val original_name: String = "",
-    val popularity: Double = 0.0,
-    val profile_path: String = "",
-    val cast_id: Int = 0,
-    val character: String = "" ,
-    val credit_id: String = "",
-    val order: Int = 0
-)
+    val cast: List<Cast> = emptyList(),
+    val id: Int = 0
+) : Parcelable
